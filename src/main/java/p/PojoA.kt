085c3cce350588f9e8278fe362cdfd053c38b85b
Rüdiger zu Dohna <snackbox@sinntr.eu>
2019-02-15 05:40:36 +0100

    kotlinized pojs

---
+++ src/main/java/p/PojoA.kt
@@ -0,0 +1,7 @@
+package p
+
+class PojoA {
+    var foo: String? = null
+    var bar: String? = null
+    var isPrimitiveBool: Boolean = false
+}
---
+++ src/main/java/p/PojoB.kt
@@ -0,0 +1,7 @@
+package p
+
+class PojoB {
+    var fox: String? = null
+    var bax: String? = null
+    var isPrimitiveBool: Boolean = false
+}
